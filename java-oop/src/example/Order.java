989cb765e936f604232bb6f66449f6f90ddd4b6c
박예찬 <jkyyc3@gmail.com>
2022-03-10 17:51:32 +0900

    CREATE : Objext Example

---
+++ java-oop/src/example/Order.java
@@ -0,0 +1,9 @@
+package example;
+
+public class Order {
+	int orederId;
+	String buyerId;
+	String sellerId;
+	int ProductId;
+	String orderDate;
+}
---
+++ java-oop/src/example/UserInfo.java
@@ -0,0 +1,11 @@
+package example;
+
+public class UserInfo {
+
+	String userId;
+	String userPassword;
+	String userName;
+	String address;
+	int phoneNumber;
+	
+}
